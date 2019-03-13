03608dca214068d51f3987e0c8e0c5c2827165e4
Jamel Hendricks <jamelmhendricks@gmail.com>
2019-03-13 14:58:50 -0400

    inital commit

---
+++ BridgesDoublyLinkedList.java
@@ -0,0 +1,26 @@
+package com.gradescope.intlist;
+
+import bridges.base.DLelement;
+import bridges.connect.Bridges;
+import com.gradescope.intlist.MyListInterface;
+
+/**
+ * This is the class skeleton that you should use for Project 4
+ *   @author < Add your name here >
+ *   Computer Science Department
+ *   College of Engineering
+ *   Virginia Commonwealth University
+ */
+
+public class BridgesDoublyLinkedList<E> implements MyListInterface<E> {
+	
+
+	 
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		
+	}
+}
